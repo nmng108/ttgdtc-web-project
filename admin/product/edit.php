ba7880cc8b6a2a6bd87707e5c9fc891620230697
nmng108 <80725916+nmng108@users.noreply.github.com>
2022-05-27 10:46:47 +0700

    Update 3.0: Merge 'namng'

---
+++ admin/product/edit.php
@@ -0,0 +1,84 @@
+<?php
+$title = 'Cập nhật thông tin dụng cụ';
+$root_dir = '../..';
+
+include_once("$root_dir/admin/layouts/header.php");
+
+// Handle form inputs sent from this file.
+if(!empty($_POST)){
+    $query = 
+		"UPDATE products 
+		SET availableQuantity = '" . $_POST['availableQuantity'] . "', 
+        itemName = '" . $_POST['itemname'] . "',
+        description = '" . $_POST['description'] . "'
+			WHERE itemCode = ".$_GET['id'].""
+		;
+	$request = run_mysql_query($query);
+	if ($request === true) {
+		header('Location: ./');
+	} else {
+		die("Cannot update the products");
+	}
+}
+?>
+<br>
+<br>
+
+<?php
+if (isset($_GET['id'])) {
+	$requestNumber = $_GET["id"];
+} else {
+	exit("ITEM CODE IS NULL");
+}
+
+
+if($requestNumber != '') {
+	$query = "SELECT * FROM products
+			WHERE itemCode = $requestNumber";
+	$request = run_mysql_query($query)->fetch_all(MYSQLI_ASSOC);
+
+	if(count($request) == 1) {
+		$request = $request[0];
+	} else {
+		?>
+		<script>
+			console.log("NULL RESULT");
+		</script>
+		<?php
+	}
+}
+
+// Get all status to create status options.
+$sql = "SELECT * FROM products";
+$all_status = run_mysql_query($sql)->fetch_all(MYSQLI_ASSOC);
+?>
+
+<div class="row" style="margin-top: 20px;">
+	<div class="col-md-12 table-responsive">
+		<h3>Trạng thái yêu cầu</h3>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+			</div>
+			<div class="panel-body">
+				<form method="post">
+                    <div class="form-group">
+                    <label for="usr">Tên:</label>
+						<input type="text" class="form-control" name="itemname" value="<?=$request['itemName']?>" required>
+					</div>
+                    <div class="form-group">
+                    <label for="usr">Mô tả:</label>
+						<input type="text" class="form-control" name="description" value="<?=$request['description']?>" required>
+					</div>
+                    <div class="form-group">
+						<label for="usr">Số lượng:</label>
+						<input type="text" class="form-control" name="availableQuantity" value="<?=$request['availableQuantity']?>" required>
+					</div>
+					<button class="btn btn-success">Xác nhận</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+require_once('../layouts/footer.php');
+?>
---
+++ database/config.php
@@ -0,0 +1,14 @@
+<?php
+// Will be merged into 'namng-branch2' in update 3.0
+
+define('HOSTNAME', 'localhost:3306');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'pec_database');
+//order
+define('ASCENDING', 'ASC');
+define('DESCENDING', 'DESC');
+
+define('SPORT_EQUIPMENT', 'SPORT_EQUIPMENT');
+define('UNIFORM', 'UNIFORM');
+define('PRIVATE_KEY', 'sjdgfsdj(*&*&6234jhsdgfjhsdsdfk&*^UUUdd');
